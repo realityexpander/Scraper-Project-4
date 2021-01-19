6476624d8abf7cd36aa6666d0306f7815ffa945d
Chris Athanas <chris@crowdskout.com>
2021-01-19 00:05:44 -0600

    Reformat with Prettier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 // Puppeteer docs
 // https://developers.google.com/web/tools/puppeteer
@@ -9,17 +9,17 @@ const puppeteer = require('puppeteer');
 // https://github.com/puppeteer/puppeteer/issues/4752
 // https://support.apple.com/en-gb/guide/keychain-access/kyca2686/mac
 
-const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/';
+const url =
+  "https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/";
 
 (async () => {
-  
-  const browser = await puppeteer.launch({headless:true});
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url);
   // await page.screenshot({path: 'example.png'});
- 
+
   // expand all comments thread
-  let expandButtons = await page.$$('.morecomments')
+  let expandButtons = await page.$$(".morecomments");
 
   // while (expandButtons.length) {
   //   for (let button of expandButtons ) {
@@ -31,31 +31,31 @@ const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_
   // }
 
   // select all comments, scrape text and points
-  const comments = await page.$$('.entry')
-  const formattedComments = []
+  const comments = await page.$$(".entry");
+  const formattedComments = [];
   for (let comment of comments) {
     // scrape points
-    const points = await comment.$eval('.score', el => el.textContent)
-                                .catch( err => console.error("no score"))
-                                
+    const points = await comment
+      .$eval(".score", (el) => el.textContent)
+      .catch((err) => console.error("no score"));
+
     // scrape text
-    const rawText = await comment.$eval('.usertext-body', el => el.textContent)
-                              .catch( err => console.error("no user text") )
-    
+    const rawText = await comment
+      .$eval(".usertext-body", (el) => el.textContent)
+      .catch((err) => console.error("no user text"));
+
     if (points && rawText) {
       // const text = rawText.replace(/\n/g, '') // regex style
-      const text = rawText.trim()
-      formattedComments.push({points, text})
+      const text = rawText.trim();
+      formattedComments.push({ points, text });
     }
-
   }
 
-  console.log({formattedComments})
+  console.log({ formattedComments });
 
   // sort comments by points
 
   // insert into google spreadsheet
 
   await browser.close();
-
-})()
+})();
